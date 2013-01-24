69cb796b2ca304addfa1544b44a3bbce53ca0aff
Vadim <vmuhin@mtt.ru>
2013-01-24 11:57:30 +0400

    исправлены неработающие тесты

--- src/com/YouMagicPro/LKA_tests/SupportEnterMessageTest.java
+++ src/com/YouMagicPro/LKA_tests/SupportEnterMessageTest.java
@@ -12,7 +12,7 @@ public class SupportEnterMessageTest extends TestBase{
 		driver.findElement(By.xpath("//span[4]/a")).click();//нажимаем на "Поддержка"
 		driver.findElement(By.cssSelector("a.bPopup__eClose.instructions-close")).click();
     	sleep();
-    	driver.findElement(By.xpath("//div[@id='umagicproConfirmDialog-d7db61d4eaa86fd123239ffe2246b1db']/div/div[2]/table/tbody/tr/td[2]/div/span")).click();
+    	driver.findElement(By.xpath("//div[@id='umagicproConfirmDialog-bf937244a640ee341e7fcc4c86fb5fa7']/div/div[2]/table/tbody/tr/td[2]/div/span")).click();
     	sleep();
     	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//form/div/div/div/div/div")));//ожидание загрузки страницы
     	driver.findElement(By.id("bSupport_MessageText")).sendKeys("11564589132 abcs ,@!.;\\/ ");
