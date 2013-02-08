1e499fc3d715637c45acb5a8f1260a3533a5de16
Vadim <vmuhin@mtt.ru>
2013-02-08 16:14:41 +0400

    спрвлн тст збс

--- src/com/YouMagicPro/LKS_tests/PhoneChangeContactFirstNameTest.java
+++ src/com/YouMagicPro/LKS_tests/PhoneChangeContactFirstNameTest.java
@@ -7,23 +7,23 @@ import com.registration.tests.TestBaseReg;
 
 public class PhoneChangeContactFirstNameTest extends TestBaseReg{
 	@Test
-	public void  phoneCreateAndDeleteGroupTest () throws Exception {	 
+	public void  ChangeContactFirstNameTest () throws Exception {	 
 		openAndLoginLKS();
 		selenium.click("link=–едактировать контакты");//Ќажатие на пункт меню "–едактирование контактов"
 		Thread.sleep(4000);
-		selenium.click("xpath=(//a[contains(text(),'ред.')])[2]");//Ќажатие на кнопку редактировани€ контакта
+		selenium.click("//tr[4]/td[2]/a");//Ќажатие на кнопку редактировани€ контакта
 		Thread.sleep(4000);
-		selenium.type("id=edit-contact-first-name", "B1");//добавить контакт ("B1")
+		selenium.type("id=edit-contact-first-name", "t1");//добавить контакт ("t1")
 		selenium.click("id=edit-save");//сохранить
 		Thread.sleep(4000);
 		selenium.click("id=edit-submit");//сохранить
 		Thread.sleep(4000);
-		AssertJUnit.assertTrue(selenium.isTextPresent("B1"));//проверка добавлени€ контакта
+		AssertJUnit.assertTrue(selenium.isTextPresent("t1"));//проверка добавлени€ контакта
 		selenium.click("link=–едактировать контакты");//Ќажатие на пункт меню "–едактирование контактов"
 		Thread.sleep(4000);
-		selenium.click("xpath=(//a[contains(text(),'ред.')])[2]");//Ќажатие на кнопку редактировани€ контакта
+		selenium.click("//tr[4]/td[2]/a");//Ќажатие на кнопку редактировани€ контакта
 		Thread.sleep(4000);
-		selenium.type("id=edit-contact-first-name", "x1");//изменить им€ контакта
+		selenium.type("id=edit-contact-first-name", "B1");//изменить им€ контакта
 		selenium.click("id=edit-save");//сохранить
 		Thread.sleep(4000);
 		selenium.click("id=edit-submit");//сохранить
