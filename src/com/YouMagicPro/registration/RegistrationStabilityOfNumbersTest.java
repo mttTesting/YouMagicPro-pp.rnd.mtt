8374821b620325fe54f005dce8f27cfda46e0ce3
Vadim <vmuhin@mtt.ru>
2013-03-11 12:42:54 +0400

    исправлен тест регистрации

--- src/com/YouMagicPro/registration/RegistrationStabilityOfNumbersTest.java
+++ src/com/YouMagicPro/registration/RegistrationStabilityOfNumbersTest.java
@@ -10,17 +10,20 @@ public class RegistrationStabilityOfNumbersTest extends TestBaseReg{
 	public void  registrationStabilityOfNumbersTest_C14943() throws Exception {
 		open();
 		selenium.click("//li[4]/a");
-		selenium.waitForPageToLoad("30000");
-		selenium.click("link=Бронзовые");
-		selenium.click("link=Платиновые");
-		selenium.click("link=Обычные");
-		AssertJUnit.assertTrue(selenium.isTextPresent("(499)"));
+		Thread.sleep(4000);
 		selenium.click("css=span.text");
-		selenium.click("//div[@id='edit-city-wrapper']/span/ul/li[2]");
-		Thread.sleep(10000);
+		selenium.click("//div[@id='edit-city-wrapper']/span/ul/li[3]");
+		Thread.sleep(5000);
+		selenium.click("link=Бронзовые");
+		selenium.click("link=Платиновые");
+		selenium.click("link=Обычные");
+		AssertJUnit.assertTrue(selenium.isTextPresent("(383)"));
+		selenium.click("css=span.text");
+		selenium.click("//div[@id='edit-city-wrapper']/span/ul/li[7]");
+		Thread.sleep(5000);
 		selenium.click("link=Платиновые");
 		selenium.click("link=Бронзовые");
 		selenium.click("link=Обычные");
-		AssertJUnit.assertTrue(selenium.isTextPresent("(812)"));
+		AssertJUnit.assertTrue(selenium.isTextPresent("(863)"));
 	}
 }
